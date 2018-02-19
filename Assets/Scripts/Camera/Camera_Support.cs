7b2f0d771eeee83fa51a0030c23f2024442eeab2
Jacob <Bilokij@gmail.com>
2018-02-18 17:43:51 -0800

    Comments added to Camera_Support.cs/EnemyHealth.cs

--- Assets/Scripts/Camera/Camera_Support.cs
+++ Assets/Scripts/Camera/Camera_Support.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class Camera_Support : MonoBehaviour {
 
+    /*Transform is a data type to get the location of an object in 3d space.
+      Here we noted that it is a PUBLIC variable, so if you go to the unity inspector
+      you will have the opportunity to select what it's target is.
+    */
     public Transform target;
+    //Smoothing is also public and can be declaired here or in the editor
     public float smoothing = 5f;
-
+    /*Offset is a Vector3 type (Meaning it looks for 3d space point)
+      It is declaired like such Vector3 variable_name = new Vector3(x coord, y coord, z coord)
+      Here it is used to offset the camera from it's target
+    */
     Vector3 offset;
-	// Use this for initialization
+
+
+	// Use this for initialization it is called at the start of a game
 	void Start () {
+        /*Our offset is our position in 3d space minus the target position.
+         You will notice there is no object for the first transform.position.
+         When this is the case it assumes the target is the object the script is attached to.
+        */
         offset = transform.position - target.position;
 	}
 	
-	// Update is called once per frame
+	// Fixed Update is different from update as it is called once every 20 frames
 	void FixedUpdate () {
+        //Get 3d posiiton of the target + the offset
         Vector3 targetCamPos = target.position + offset;
+        /*Set our camera position to the new position
+          Lerp is used for a smooth transition between two positions
+          Here the two positions are our starting position, our new position, and then our smoothing
+          We multiply smoothing by Time.deltaTime because we want to average out the smoothing
+          Time is a constant among all computers so this will help slower and faster computers perform similar
+        */
         transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
 	}
 }
--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,51 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Import AI from unity engine for access to the navmesh component
 using UnityEngine.AI;
 
 public class EnemyHealth : MonoBehaviour {
 
+    //Public variables can be declaired here or in editor.
     public int startingHealth = 100;
     public int currentHealth;
+    //How fast enemy sinks into ground when dead.
     public float sinkSpeed = 2.5f;
 
+    //Sphere collider is used as a trigger to detect if
+    //we are in range of the enemy.
     SphereCollider sphereCollider;
     bool isDead;
     bool isSinking;
 
+    //Awake is similar to start but runs more often to avoid glitches from pausing or
+    //incorrect initalization.
     private void Awake()
     {
+        /*
+         * Get component is used to reference objects that are connected
+         * to the same gameObject that the script is connected to.
+         * Here we also have a sphere collider on our enemy
+         * so we can get access to it in code by saying
+         * GetComponent<ITEM_TO_ACCESS>();
+         */
         sphereCollider = GetComponent<SphereCollider>();
         isDead = false;
         isSinking = false;
         currentHealth = startingHealth;
     }
-
-    // Use this for initialization
-    void Start () {
-        
-	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(isSinking)
         {
+            /*
+             * If we are sinking then we move downwards.
+             * We multiply smoothing by Time.deltaTime because we want to average out the smoothing.
+             * Time is a constant among all computers so this will help slower and faster computers perform similar
+             */
             transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
         }
 	}
-
+    /// <summary>
+    /// Take damage is used by the player script to attack the enemy this script is attached to.
+    /// </summary>
+    /// <param name="amount"> Amount of damage given to enemy </param>
+    /// <param name="hitPoint"> The exact point the enemy is hit
+    /// This is used to apply affects on dmg like blood. </param>
     public void TakeDamage(int amount, Vector3 hitPoint)
     {
         if(isDead)
         {
             return;
         }
+        //Remove damage from health
         currentHealth -= amount;
+        //If health is 0 or below run death method
         if(currentHealth <= 0)
         {
             Death();
         }
     }
+    /// <summary>
+    /// Run when enemies are dead triggers a death boolean
+    /// Make sphereCollider trigger so the player can walk through dead bodies
+    /// Run StartSinking method.
+    /// </summary>
     void Death()
     {
         isDead = true;
@@ -53,11 +79,17 @@ public class EnemyHealth : MonoBehaviour {
         StartSinking();
     }
 
+    /// <summary>
+    /// Get the navmesh component and disable it to release the resources it uses
+    /// Make the body kinematic using ragdoll physics
+    /// Set sinking variable to true
+    /// </summary>
     public void StartSinking()
     {
         GetComponent<NavMeshAgent>().enabled = false;
         GetComponent<Rigidbody>().isKinematic = true;
         isSinking = true;
+        //After 2 seconds destroy object this script is attached to.
         Destroy(gameObject, 2f);
     }
 }
